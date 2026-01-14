4da8330decbf5dc103f468c388981ba8e05943d0
Ojaswi <ojaswideep2020@gmail.com>
2026-01-15 04:37:07 +0530

    error fixed

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
+// @ts-ignore - Type incompatibility between next-pwa and Next.js 16
 import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {},
 };
 
-export default withPWA({
+// @ts-ignore
+const configWithPWA = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-})(nextConfig);
+// @ts-ignore
+})(nextConfig as any);
+
+export default configWithPWA;
